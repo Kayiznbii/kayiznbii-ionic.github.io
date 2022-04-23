3406c4968c76a1e8c1f26e4d75dc41fe3a69de9b
Kalyn <kalyneisenbeis@u.boisestate.edu>
2022-04-22 22:27:42 -0600

    created new page

--- MobileApp/turn-tracker/src/app/app-routing.module.ts
+++ MobileApp/turn-tracker/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'create-party',
+    loadChildren: () => import('./create-party/create-party.module').then( m => m.CreatePartyPageModule)
   }
 ];
 @NgModule({
